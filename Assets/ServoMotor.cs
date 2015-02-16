fd8f89fd479309a7b83ebd7844ec987cc299c371
Alyona Manyuhina <technogenom@gmail.com>
2015-02-16 17:27:45 +0300

    16/02/15
    Working on rotor.
    
    Signed-off-by: Alyona Manyuhina <technogenom@gmail.com>

---
+++ Assets/ServoMotor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServoMotor : MonoBehaviour {
+
+	public Rigidbody child;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
